88497486b808c71a10b3852e5b02b81a61a40bdf
svt2404@gmail.com <svt2404@gmail.com@a5288802-4a3a-ea87-de81-b29a806184f5>
2012-03-04 15:39:30 +0000

    Обработчик входных данных. Приношу извинения за русские комментарии.

---
+++ trunk/src/game/input/Separator.java
@@ -0,0 +1,31 @@
+package game.input;
+import java.io.*;
+public class Separator 
+{
+	private BufferedReader bufreader;
+	private byte [][] mas;
+	public static int X, Y; // количество моноблоков по высоте и ширине
+		
+	public byte[][] interpritation(String name) throws IOException // преобразуем входные данные в byte
+	{
+		bufreader = new BufferedReader(new FileReader(name));
+		String read [] = bufreader.readLine().split(" ");
+		X = Integer.valueOf(read[0]);
+		Y = Integer.valueOf(read[1]);
+		mas = new byte[X][Y];
+		for(int i = 0; i < X; i++)
+		{
+			String s = bufreader.readLine();
+			for(int g = 0; g < Y; g++)
+			{
+				char a = s.charAt(g);
+				switch(a)
+				{
+				case 'o': mas[i][g] = 0;
+				case 'w': mas[i][g] = 1;
+				}
+			}
+		}
+		return mas;
+	}
+}
