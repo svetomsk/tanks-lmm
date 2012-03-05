56448e70e721dc8c22a9a74f4bccfbe70e50ef2e
svt2404@gmail.com <svt2404@gmail.com@a5288802-4a3a-ea87-de81-b29a806184f5>
2012-03-05 10:25:19 +0000

    Добавил методы для обработки событий кнопок меню. Когда будет что выполнять при нажатии, нужно будет просто добавить нужные действия в эти методы. Пока какие-либо действия выполняет только кнопка Exit.

--- trunk/src/game/main/TanksActivity.java
+++ trunk/src/game/main/TanksActivity.java
@@ -3,12 +3,29 @@ import game.graphics.*;
 import game.tanks.R;
 import android.app.Activity;
 import android.os.Bundle;
-import java.io.*;
+import android.view.View;
+
 public class TanksActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);        
+    }
+    
+    public void newgameButtonEvent(View w)
+    {
+    	// actions to start new game
+    }
+    
+    public void aboutButtonEvent(View w)
+    {
+    	//showing information about game
+    }
+    
+    public void exitButtonEvent(View w)
+    {
+    	// may be saving or simply exit
+    	System.exit(0);
     }
 }
