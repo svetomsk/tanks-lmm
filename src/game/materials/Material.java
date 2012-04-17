0e0fc603f33057918721870fd0fef31d5848ea94
svt2404@gmail.com <svt2404@gmail.com@a5288802-4a3a-ea87-de81-b29a806184f5>
2012-04-17 10:00:27 +0000

    Добавили материалы. Теперь доступно масштабирование, зависящее лишь от одного коээфецента.

---
+++ src/game/materials/Material.java
@@ -0,0 +1,31 @@
+package game.materials;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Picture;
+import android.graphics.drawable.Drawable;
+
+public class Material {
+	private int hardness;
+	private boolean isExplodable;
+	private boolean isPermeableToLight;
+	private boolean isDrivable;
+	private Bitmap texture;
+		
+	public Material(int hardness, boolean isExplodable, boolean isPermeableToLight, boolean isDrivable, Bitmap texture)
+	{
+		this.hardness = hardness;
+		this.isExplodable = isExplodable;
+		this.isPermeableToLight = isPermeableToLight;
+		this.isDrivable = isDrivable;
+		this.texture = texture;		
+	}
+	
+	public int getHardness() { return hardness; }
+	public boolean isExplodable() { return isExplodable; }
+	public boolean isPermeableToLight() { return isPermeableToLight; }
+	public boolean isDrivable () { return isDrivable; }
+	public Bitmap getTexture() { return texture; }
+	
+	//юзай extend
+}
