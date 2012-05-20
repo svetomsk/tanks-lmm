8c94b0492b7178de1f113746897e4b54b394f1bd
kvas.omsk@gmail.com <kvas.omsk@gmail.com@a5288802-4a3a-ea87-de81-b29a806184f5>
2012-05-20 05:47:13 +0000

    Теперь работает на моем телефоне. Прикрутил насильный парсинг bmpшек в формат ARGB_8888 и добавил констант для материалов.

--- trunk/src/game/input/Separator.java
+++ trunk/src/game/input/Separator.java
@@ -1,58 +1,66 @@
 package game.input;
+
+import game.materials.Materials;
 import game.tanks.R;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-public class Separator 
-{
-	private byte [][] mas;
-	public static int X, Y; // кол-во по ширине и высоте
-	private final Resources r; // объект для обращения к ресурсам
-		
-	public Separator(Resources res)
-	{
-		r = res;
+
+public class Separator {
+    private byte[][] mas;
+    public static int X, Y; // кол-во по ширине и высоте
+    private final Resources r; // объект для обращения к ресурсам
+
+    public Separator(Resources res) {
+	r = res;
+    }
+
+    // цвета с карты в формате ARGB
+    private final int COLOR_BRICK = 0xffb97a57, COLOR_FUCKINBLUE = 0xff99d9ea,
+	    COLOR_FUCKINGRAY = 0xff7f7f7f;
+
+    private byte decodeColor(int c) {
+	switch (c) {
+	case COLOR_FUCKINGRAY:
+	    return Materials.MAT_BLOCK_ADMINIUM;
+	case COLOR_BRICK:
+	    return Materials.MAT_BLOCK_BRICK;
+	case COLOR_FUCKINBLUE:
+	    return Materials.MAT_BLOCK_GRASS;
+	default:
+	    return 0;
 	}
-//	private final int COLOR_1;
-//	private final int COLOR_2;
-	
-	public byte[][] interpritaitonPicture(int name) // интерпретируем картинку в массив byte
-	{
-		Bitmap pic = null;
-		if(name == 1) { pic = BitmapFactory.decodeResource(r, R.drawable.map1); }
-		//if(name == 2) { pic = BitmapFactory.decodeResource(r, R.drawable.map2); }
-		
-		Y = pic.getHeight();
-		X = pic.getWidth();
-		mas = new byte[X+2][Y+2];
-		
-		for(int q=0;q<X;q++)
-		{
-			for(int w=0;w<Y;w++)
-			{
-				mas[q+1][w+1] = 0;
-				switch(pic.getPixel(q, w))
-				{
-				case -8421505: {mas[q+1][w+1] = 1; break;}
-				case -4621737: {mas[q+1][w+1] = 2; break;}
-				case -6694422: {mas[q+1][w+1] = 4; break;}
-				}
-			}
-		}
-		// заполняем рамку
-		// по вертикали
-		for(int q=0;q<X+2;q++)
-		{
-			mas[q][0] = 1;
-			mas[q][Y+1] = 1;			
-		}
-		// по горизонтали
-		for(int q=0;q<Y+2;q++)
-		{
-			mas[0][q] = 1;
-			mas[X+1][q] = 1;			
-		}
-		
-		return mas;
+
+    }
+
+    public byte[][] interpritaitonPicture(int name) // интерпретируем картинку в
+						    // массив byte
+    {
+	Bitmap pic = null;
+	BitmapFactory.Options opt = new BitmapFactory.Options();
+	opt.inPreferredConfig = Bitmap.Config.ARGB_8888;
+	if (name == 1) {
+	    pic = BitmapFactory.decodeResource(r, R.drawable.map1, opt);
 	}
+	// if(name == 2) { pic = BitmapFactory.decodeResource(r,
+	// R.drawable.map2); }
+
+	Y = pic.getHeight();
+	X = pic.getWidth();
+	mas = new byte[X + 2][Y + 2];
+
+	for (int q = 0; q < X; q++) 
+	    for (int w = 0; w < Y; w++) 
+		mas[q+1][w+1] = decodeColor(pic.getPixel(q, w));
+	    
+	// заполняем рамку
+	// по вертикали
+	for (int q = 0; q < X + 2; q++)
+	    mas[q][0] = mas[q][Y + 1] = 1;
+	// по горизонтали
+	for (int q = 0; q < Y + 2; q++)
+	    mas[0][q] = mas[X + 1][q] = 1;
+
+	return mas;
+    }
 }
--- trunk/src/game/materials/Materials.java
+++ trunk/src/game/materials/Materials.java
@@ -8,13 +8,19 @@ public class Materials {
 	
 	private Material[] arr = new Material[5];
 	
+	public static final int MAT_FLOOR_DEFAULT = 0,
+		 	    MAT_BLOCK_ADMINIUM = 1,
+		            MAT_BLOCK_BRICK = 2,
+		            MAT_FLOOR_GRASS = 3,
+		            MAT_BLOCK_GRASS = 4;		 
+	
 	public void init(Resources r)
 	{
-		arr[0] = new Material(1, false, true, true, BitmapFactory.decodeResource(r, R.drawable.floor_default));
-		arr[1] = new Material(9999, true, false, false, BitmapFactory.decodeResource(r, R.drawable.block_adminium));
-		arr[2] = new Material(8, true, false, false, BitmapFactory.decodeResource(r, R.drawable.block_brick));
-		arr[3] = new Material(1, false, true, true, BitmapFactory.decodeResource(r, R.drawable.floor_grass));
-		arr[4] = new Material(1, true, true, false, BitmapFactory.decodeResource(r, R.drawable.block_glass));
+		arr[MAT_FLOOR_DEFAULT] = new Material(1, false, true, true, BitmapFactory.decodeResource(r, R.drawable.floor_default));
+		arr[MAT_BLOCK_ADMINIUM] = new Material(9999, true, false, false, BitmapFactory.decodeResource(r, R.drawable.block_adminium));
+		arr[MAT_BLOCK_BRICK] = new Material(8, true, false, false, BitmapFactory.decodeResource(r, R.drawable.block_brick));
+		arr[MAT_FLOOR_GRASS] = new Material(1, false, true, true, BitmapFactory.decodeResource(r, R.drawable.floor_grass));
+		arr[MAT_BLOCK_GRASS] = new Material(1, true, true, false, BitmapFactory.decodeResource(r, R.drawable.block_glass));
 	}
 	public Material get(int i)
 	{
